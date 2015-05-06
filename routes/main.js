2e22fdcd28fb839a8326b8b1a022659dd8572a4f
Sean C. Ryan <piefayth@gmail.com>
2015-05-05 22:38:37 -0500

    Completed administrative login / forbidding unauthorized users from edits

--- routes/main.js
+++ routes/main.js
@@ -1,41 +1,37 @@
 var Project = require('../models/project.js');
 var bodyParser = require('body-parser');
+var bcrypt = require('bcrypt');
+var session = require('express-session');
 
 module.exports = function(app){
 
   app.use(bodyParser.json());
+  app.use(session({ secret: 'thisismysecrettherearemanylikeitbutthisoneismine', resave: false, saveUninitialized: false }));
+  var parse = bodyParser.urlencoded({extended: false});
+
+
+  /*
+  Use the following to generate a password for the one and only one user.
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync('yourpassword', salt);
+  */
 
   app.get('/', function(req, res){
     res.sendFile('index.html', {root: __dirname + '/../views/'});
-
-    /*var project = new Project({
-        name: "Photo URL Integrity Tool",
-        description: "This tool batch-requests a set of image URLs as defined in a list or file, then returns them to the client as they are processing via server sent events.",
-        imageurl: "img/Capture.PNG",
-        demourl: "http://seanryan.tips/photourls/",
-        browsers: [
-            "Firefox",
-            "Internet Explorer",
-            "Chrome",
-            "iOS"
-        ],
-        source: "https://github.com/Piefayth/angular-portfolio",
-        builton: [
-            "node.js",
-            "Bootstrap",
-            "jQuery"
-        ],
-        notes: "A variable controls the amount of concurrent urls that the server will process to prevent 100% server resource usage from a single client.",
-        mechanism: "After parsing the URLs from the provided input, this tool attempts to access each resource. It returns a pass or fail based on the http status code and the MIME type of the body of the response.",
-        mode: "normal",
-        cache: null
-    });
-
-    project.save(function(err){
-      if(err) console.log(err);
-    });*/
-
   });
+
+  app.get('/admin', function(req, res){
+    res.sendFile('admin.html', {root: __dirname + '/../views/'});
+  })
+
+  app.post('/admin', parse, function(req, res){
+    if(bcrypt.compareSync(req.body.password, '$2a$10$DZruKtzIvqtsKzz8Vm7xeemGI.TqjU25y31g1qkbkSVsJA4fOTpte')){
+      req.session.auth = true;
+      res.redirect('/');
+    } else {
+      res.end();
+    }
+  })
 
   app.get('/normal-project', function(req, res){
     res.sendFile('normal-project.html', {root: __dirname + '/../views/'});
@@ -59,13 +55,16 @@ module.exports = function(app){
   app.post('/api/projects', function(req, res){
 
     //create a new project
-
-    var project = new Project(req.body);
-    project.mode = 'normal';
-    project.save(function(err){
-      if (err) console.log(err);
-      res.json(project);
-    })
+    if(req.session && req.session.auth){
+      var project = new Project(req.body);
+      project.mode = 'normal';
+      project.save(function(err){
+        if (err) console.log(err);
+        res.json(project);
+      })
+    } else {
+      res.end();
+    }
 
   })
 
@@ -74,24 +73,27 @@ module.exports = function(app){
   })
 
   app.put('/api/projects/:project', function(req, res){
-    console.log(req.body)
-    var project = req.body;
+    if(req.session && req.session.auth){
+      var project = req.body;
 
-    Project.findOne({ id: project.id }, function(err, dbUpdateProject){
-      for(var key in project){
-        if(project[key] && dbUpdateProject[key]){
-          dbUpdateProject[key] = project[key];
+      Project.findOne({ id: project.id }, function(err, dbUpdateProject){
+        for(var key in project){
+          if(project[key] && dbUpdateProject[key]){
+            dbUpdateProject[key] = project[key];
+          }
         }
-      }
-      dbUpdateProject.save();
-    })
+        dbUpdateProject.save();
+      })
+    } else {
+      res.end();
+    }
   })
 
   app.delete('/api/projects/:project', function(req, res){
     //deletes one project by id req.params.project
-
-    Project.find({id: req.params.project.id}).remove().exec();
-
+    if(req.session && req.session.auth){
+      Project.find({id: req.params.project.id}).remove().exec();
+    }
     res.end();
   })
 
